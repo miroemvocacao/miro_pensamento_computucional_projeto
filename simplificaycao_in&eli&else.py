e932f52e2f5f76984c1ba8309ef52406a830f59d
miroemvocacao <geladomiro2.0@gmail.com>
2026-03-09 17:00:21 -0300

    update
    1-teste de idade
    2-projeto do salão
    3- a simplificação do if,eli e else

---
+++ simplificaycao_in&eli&else.py
@@ -0,0 +1,17 @@
+'''
+
+
+'''
+preco_frutas = {
+    'macã': 2.5,
+    'banana': 1.8,
+    'laranja': 3.0
+}
+
+
+fruta_desejada = 'macã'
+
+
+resultado = preco_frutas.get(fruta_desejada, 'Fruta não encontrada')
+
+print(f"o preco da {fruta_desejada} é: {resultado}")
---
+++ teste_idade.py
@@ -0,0 +1,15 @@
+'''
+
+
+'''
+
+print('digite a idade do jovem')
+
+idade = int(input())
+
+if idade < 18:
+    print('jovem é menor de idade')
+elif idade >= 60:
+    print('jovem é idoso de idade')
+else:
+    print('jovem é maior de idade')
